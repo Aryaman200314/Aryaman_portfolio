17f71cfb2fd727a27ada15b451145b867f0c2ede
Aryaman Sharma <200314arya@gmail.com>
2024-09-20 21:07:42 +0530

    Updated link

--- src/Components/MySelf/Myself.js
+++ src/Components/MySelf/Myself.js
@@ -4,6 +4,9 @@ import myself from '../../assets/myself.jpg'
 import user_alt from '../../assets/user_alt.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 function Myself() {
+  const openResume = () => {
+    window.open('https://drive.google.com/file/d/1LxQciGHDS2Iy2WeLAx6buwcp8xr4DmzY/view?usp=sharing', '_blank'); // Opens the resume in a new tab
+  };
   return (
    <div id='home' className='my-self'>
     <img className='my-self-img' src= {myself} alt={user_alt} />
@@ -11,7 +14,7 @@ function Myself() {
     <p>Pursuing CSE @LPU | C++ | HTML | CSS | Javascript | React.js | Git | Docker | AWS </p>
     <div className='my-self-btns'>
         <div className='connect-btn'><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className='my-resume'>My Resume</div>
+        <button onClick={openResume} className='my-resume'>My Resume</button>
     </div>
    </div>
   )
