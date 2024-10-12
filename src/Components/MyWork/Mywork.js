b7e48b69c91d656e21fdbf20ae7b7e18c3a01f64
Aryaman Sharma <200314arya@gmail.com>
2024-10-13 00:59:00 +0530

    mywork

--- src/Components/MyWork/Mywork.js
+++ src/Components/MyWork/Mywork.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import './Mywork.css';
-// import alt_img from './alt_img.png'; 
-// import My_work_data from '../../assets/My_word_data/My_work_data.js';
-import HourglassTopIcon from '@mui/icons-material/HourglassTop';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-
+import Myproject from '../../assets/My_word_data/Myproject.js'
 const Mywork = () => {
   return (
     <div id='projects' className='my-work'>
       <div className='my-work-heading'>
         <h1>My latest work</h1>
-        <hr className='hr-my-work'/>
+        <hr className='hr' />
       </div>
-      <div className='my-work-container'>
-
-        {/* {My_work_data.map((work, index) => (
-          <img
-            key={index}
-            src={work.w_img}
-            alt={alt_img || 'My Work Image'}
-            className='my-work-images'
-          />
-        ))} */}
-      </div>
-      <div className='my-work-show-more'>
-      <HourglassTopIcon />
-        {/* <p>Show More <ChevronRightIcon /></p> */}
+      <div className='projects-container'>
+        {Myproject.map((projects, index) => {
+          return (
+            <div key={index} className='projects-format'>
+              <h2>{projects.w_no}</h2>
+              <h3>{projects.w_name}</h3>
+              <p>{projects.w_desc}</p>
+              <hr />
+              <img src={projects.w_img} alt='Images coming soon' className='my-work-images' />
+              
+              <div className='services-readmore'>
+                <p className='webpage-link'>Link to Website</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
---
+++ src/assets/My_word_data/Myproject.js
@@ -0,0 +1,35 @@
+
+import Todo from './Todo.png';
+import Typingninja from './Typingninja.png'
+import Weather from './Weather.png'
+const Myproject = [
+
+    {
+        w_no: "01",
+        w_name: "To-DO List",
+        w_img: {Todo},
+        w_desc: "A dynamic to-do list app with task management across Backlog, To Do, Ongoing, and Done stages."
+    },
+    {
+        w_no: "02",
+        w_name: "Typing Ninja",
+        w_img: {Typingninja},
+        w_desc: "An online typing game that challenges the user to type the correct word in a given time limit."
+    },
+    {
+        w_no: "03",
+        w_name: "Weather App",
+        w_img: {Weather},
+        w_desc: "An app that provides the current weather details of any city in the world."   
+    },
+    {
+        w_no: "04",
+        w_name: "Notify",
+        w_img: {},
+        w_desc: "A note-taking app with user authentication, search, tagging, pinned notes, responsive design, dark/light themes, and efficient note management." 
+
+    }
+
+
+]
+export default Myproject;
